121071f7581c125af60a6e2b7b04701301405685
github-action-benchmark <github@users.noreply.github.com>
2026-04-20 08:42:16 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 67c8fe521fadbad429de8358cce8c39d60b48ea3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776356333291,
+  "lastUpdate": 1776674536658,
   "repoUrl": "https://github.com/gildub/trustify",
   "entries": {
     "Benchmark": [
@@ -15223,6 +15223,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Ingest DS3",
             "value": 7,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "committer": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "distinct": true,
+          "id": "67c8fe521fadbad429de8358cce8c39d60b48ea3",
+          "message": "chore: prepare release 0.5.0-beta.1",
+          "timestamp": "2026-04-17T14:46:38Z",
+          "tree_id": "b54312e205aa175b645e26d48b6b88cf614112f0",
+          "url": "https://github.com/gildub/trustify/commit/67c8fe521fadbad429de8358cce8c39d60b48ea3"
+        },
+        "date": 1776674535114,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ingest DS3",
+            "value": 9,
             "unit": "s"
           }
         ]
