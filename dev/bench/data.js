4e29bf82fe4f5d21d4d3ba638f1272f3b60e45ef
github-action-benchmark <github@users.noreply.github.com>
2026-06-16 10:59:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 628aca46b7893fd206d843d527914f001c166344

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1781175942503,
+  "lastUpdate": 1781607562772,
   "repoUrl": "https://github.com/gildub/trustify",
   "entries": {
     "Benchmark": [
@@ -15508,6 +15508,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gildub/trustify/commit/d00dd3421a78eb63ca48b93d402261d3a6ceaf83"
         },
         "date": 1781175940929,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ingest DS3",
+            "value": 9,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mdessi@redhat.com",
+            "name": "desmax74",
+            "username": "desmax74"
+          },
+          "committer": {
+            "email": "desmax74@users.noreply.github.com",
+            "name": "Massimiliano Dessì - (Fast Chauffeur)",
+            "username": "desmax74"
+          },
+          "distinct": true,
+          "id": "628aca46b7893fd206d843d527914f001c166344",
+          "message": "Refactor to centralize configuration\n\nSigned-off-by: desmax74 <mdessi@redhat.com>",
+          "timestamp": "2026-06-15T14:25:52Z",
+          "tree_id": "f3ae31cf01069b51b39666196ea461b204ac2037",
+          "url": "https://github.com/gildub/trustify/commit/628aca46b7893fd206d843d527914f001c166344"
+        },
+        "date": 1781607560341,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
