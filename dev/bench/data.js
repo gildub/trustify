b397fa477df36086072749825b73d1a30512941b
github-action-benchmark <github@users.noreply.github.com>
2026-09-18 15:56:15 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 2087f226db75a3c7673f3c71551a2f0e91346d00

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789666147897,
+  "lastUpdate": 1789746975435,
   "repoUrl": "https://github.com/gildub/trustify",
   "entries": {
     "Benchmark": [
@@ -15827,6 +15827,35 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/gildub/trustify/commit/ba9c3ab5e0ec181021420f25c1ed7066347b872f"
         },
         "date": 1789666145677,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ingest DS3",
+            "value": 9,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "gilles@redhat.com",
+            "name": "gildub",
+            "username": "gildub"
+          },
+          "committer": {
+            "email": "gilles@redhat.com",
+            "name": "Gilles Dubreuil",
+            "username": "gildub"
+          },
+          "distinct": true,
+          "id": "2087f226db75a3c7673f3c71551a2f0e91346d00",
+          "message": "fix(openapi): use /purl/recommend/report",
+          "timestamp": "2026-09-18T13:08:36Z",
+          "tree_id": "b3ed0feb8f9235f79d41bc9a2eb0ff621c5b637e",
+          "url": "https://github.com/gildub/trustify/commit/2087f226db75a3c7673f3c71551a2f0e91346d00"
+        },
+        "date": 1789746973180,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
